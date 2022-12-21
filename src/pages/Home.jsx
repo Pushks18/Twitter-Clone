3a820f4ce5c8c397726a0b353b747f36a2933c0a
= <Pushks2015@gmail.com>
2022-12-21 16:02:25 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import Sidebar from '../components/Sidebar'
+import React from "react";
+import Layout from "../components/Layout";
+import Sidebar from "../components/Sidebar";
+import HomeFeed from "../components/HomeFeed";
+import ExploreBar from "../components/ExploreBar";
 
 const Home = () => {
   return (
     <div>
-        <Layout title="Home / Twitter">
-            <div className='h-screen flex flex-row'>
-            <Sidebar />
-            </div>
-        </Layout>
-    </div>
-  )
-}
+      <Layout title="Home / Twitter">
+        <div className="h-screen flex flex-row">
+          <Sidebar />
 
-export default Home
+          <div className="flex-1">
+            <HomeFeed />
+          </div>
+
+          <ExploreBar />
+        </div>
+      </Layout>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import ProfileInfo from "../components/ProfileInfo";
+import Layout from "../components/Layout";
+import Sidebar from "../components/Sidebar";
+import ExploreBar from "../components/ExploreBar";
+import profileData from "../data/profileData.json";
+import TweetCard from "../components/TweetCard";
+import tweetData from "../data/tweets.json";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const profile = profileData;
 
-export default Profile
+  const allTweets = tweetData.map((item) => {
+    return <TweetCard key={item.id} tweetData={item} />;
+  });
+  return (
+    <div>
+      <Layout title="Pushkaraj Baradkar / Twitter">
+        <div className="h-screen flex flex-row">
+          <Sidebar />
+
+          <div className="flex-1 overflow-scroll">
+            <ProfileInfo profile={profile} />
+            {allTweets}
+          </div>
+
+          <ExploreBar />
+        </div>
+      </Layout>
+    </div>
+  );
+};
+
+export default Profile;
